b7afbb74326edeb7416eceaecbb59d149ea11dff
voltarou <bagusadnan04@gmail.com>
2024-03-08 12:11:27 +0800

    Add project files.

---
+++ 101_Bubblespace/101_Bubblespace.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+    while (true) {
+        cout << "Masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "/nArray dapat mempunyai maksimal 20 elemen. /n";
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
+
+void bubbleSortArray() {
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp;
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+        pass = pass + 1;
+
+    } while (pass <= n - 1);
+}
